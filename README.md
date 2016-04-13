f71a458574b25636740ee55dd06cac50a414c473
Luis Elizondo <luis.elizondo@mx1.ibm.com>
2016-04-13 18:13:17 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Message Hub Node
+Message Hub node can produce/consume the mesasges to/from Message Hub along with a topic on [NodeRED](http://nodered.org).
+
+
+## Install
+Install from [npm](http://npmjs.org)
+```
+npm install -g node-red-contrib-messagehub-node
+```
+
+## Prerequisites
+ * Node-RED platform. (see [Node-RED](http://nodered.org/docs/getting-started/installation.html))
+ * A Bluemix Account and a Message Hub Service (see [Message Hub](https://console.ng.bluemix.net/docs/services/MessageHub/index.html#messagehub)).
+ * A topic created on the Message Hub Dashboard
+
+## Authors
+* Luis Elizondo - [luis.elizondo@mx1.ibm.com](mailto:fwang1@us.ibm.com)
+
+## License
+Released under the Apache License 2.0
---
+++ messagehub.js
@@ -0,0 +1,115 @@
+/**
+ * Created by fwang1 on 3/25/15.
+ */
+module.exports = function(RED) {
+  /*
+   *   MessageHub Producer
+   */
+  function MessageHubProducer(config) {
+    RED.nodes.createNode(this, config);
+
+    var node = this;
+    var MessageHub = require('message-hub-rest');
+
+    var apikey = config.apikey;
+    var kafka_rest_url = config.kafkaresturl;
+    var services = {
+      "messagehub": [
+        {
+          "credentials": {
+            "api_key": apikey,
+            "kafka_rest_url": kafka_rest_url
+          }
+        }
+      ]
+    }
+
+    var instance = new MessageHub(services);
+    var topic = config.topic;
+
+    try {
+      this.on("input", function(msg) {
+        var payloads = [];
+
+        node.log(msg.payload);
+        payloads.push(msg.payload);
+
+        var list = new MessageHub.MessageList(payloads);
+
+        instance.produce(topic, list.messages)
+        .then(function(data) {
+          node.log("Message sent");
+          node.log(data);
+        })
+        .fail(function(error) {
+          node.error(error);
+        });
+      });
+    }
+    catch(e) {
+      node.error(e);
+    }
+  }
+
+  RED.nodes.registerType("messagehub out", MessageHubProducer);
+
+  /*
+   * Message Hub Consumer
+   */
+  function MessageHubConsumer(config) {
+    RED.nodes.createNode(this,config);
+
+    var node = this;
+    var MessageHub = require('message-hub-rest');
+    var apikey = config.apikey;
+    var kafka_rest_url = config.kafkaresturl;
+    var services = {
+      "messagehub": [
+        {
+          "credentials": {
+            "api_key": apikey,
+            "kafka_rest_url": kafka_rest_url
+          }
+        }
+      ]
+    }
+
+    var instance = new MessageHub(services);
+    var topic = config.topic;
+    var consumerInstance;
+
+    function random() {
+      return Math.floor((Math.random() * 100) + 1);
+    }
+
+    node.log(topic);
+    instance.consume('nodered-' + topic + "-" + random(), 'nodered', { 'auto.offset.reset': 'largest' })
+    .then(function(response) {
+      consumerInstance = response[0];
+    })
+    .fail(function(error) {
+      node.error(error);
+    });
+
+    try {
+      this.log("Consumer created...");
+      setInterval(function() {
+        consumerInstance.get(topic)
+        .then(function(data) {
+          for(var i=0; i<data.length; i++) {
+            node.send({payload: data[i]});
+          }
+        })
+        .fail(function(err) {
+          node.error(err);
+        });
+      }, 2000);
+    }
+    catch(e){
+      node.error(e);
+      return;
+    }
+  }
+
+  RED.nodes.registerType("messagehub in", MessageHubConsumer);
+};
