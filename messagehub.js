37baab7331f67918ffee8be2dc34f983f9e52681
Nicolas Cisco <nicolas.cisco@semperti.com>
2017-06-27 14:24:41 -0300

    Fixing ui loading. Fixed interval

--- messagehub.js
+++ messagehub.js
@@ -1,38 +1,52 @@
 /**
  * Created by fwang1 on 3/25/15.
  */
+
+var MessageHub = require('message-hub-rest');
+var Q = require('q');
+
+function createServices(config) {
+  return {
+    "messagehub": [
+      {
+        "credentials": {
+          "api_key":  config.apikey,
+          "kafka_rest_url": config.kafkaresturl
+        }
+      }
+    ]
+  }
+}
+
 module.exports = function(RED) {
-    var MessageHub = require('message-hub-rest'),
-        vcap = JSON.parse(process.env.VCAP_SERVICES || "{}"),
-        services = null,
-        servicesList = null;
-    services = vcap["messagehub"] || [];
+    var vcap = JSON.parse(process.env.VCAP_SERVICES || "{}");
+    var services = vcap["messagehub"] || [];
 
     // make these names available to the node configuration
     RED.httpAdmin.get('/message-hub/vcap', function(req, res) {
-        res.json(services);
+      res.json(services);
     });
 
     RED.httpAdmin.get('/message-hub/service/:serviceSelectedVal/topics', function(req, res) {
-        var serviceSelectedVal = decodeURIComponent(req.params.serviceSelectedVal),
-            selectedService = services.find(function(service) {
-                return service.name == serviceSelectedVal;
-            });
-
-        if (!selectedService)
-            return res.json([]);
-
-        var instance = new MessageHub({
-            messagehub: [selectedService]
+      var serviceSelectedVal = decodeURIComponent(req.params.serviceSelectedVal),
+        selectedService = services.find(function(service) {
+            return service.name == serviceSelectedVal;
         });
 
-        instance.topics.get()
-          .then(function(response) {
-              res.json(response);
-          })
-          .fail(function(error) {
-              res.json(error)
-          });
+      if (!selectedService)
+        return res.json([]);
+
+      var instance = new MessageHub({
+        messagehub: [selectedService]
+      });
+
+      instance.topics.get()
+        .then(function(response) {
+          res.json(response);
+        })
+        .fail(function(error) {
+          res.json(error)
+        });
     });
 
   /*
@@ -42,19 +56,7 @@ module.exports = function(RED) {
     RED.nodes.createNode(this, config);
 
     var node = this;
-
-    var apikey = config.apikey;
-    var kafka_rest_url = config.kafkaresturl;
-    var services = {
-      "messagehub": [
-        {
-          "credentials": {
-            "api_key": apikey,
-            "kafka_rest_url": kafka_rest_url
-          }
-        }
-      ]
-    }
+    var services = createServices(config);
 
     var instance = new MessageHub(services);
     var topic = config.topic;
@@ -70,8 +72,8 @@ module.exports = function(RED) {
 
         instance.produce(topic, list.messages)
         .then(function(data) {
-          node.log("Message sent");
-          node.log(data);
+          node.debug("Message sent");
+          node.debug(data);
         })
         .fail(function(error) {
           node.error(error);
@@ -92,54 +94,52 @@ module.exports = function(RED) {
     RED.nodes.createNode(this,config);
 
     var node = this;
-    var apikey = config.apikey;
-    var kafka_rest_url = config.kafkaresturl;
-    var services = {
-      "messagehub": [
-        {
-          "credentials": {
-            "api_key": apikey,
-            "kafka_rest_url": kafka_rest_url
-          }
-        }
-      ]
-    }
+    var interval = parseInt(config.interval) || 2000;
+    var services = createServices(config);
 
     var instance = new MessageHub(services);
     var topic = config.topic;
-    var consumerInstance;
 
     function random() {
       return Math.floor((Math.random() * 100) + 1);
     }
 
-    node.log(topic);
-    instance.consume('nodered-' + topic + "-" + random(), 'nodered', { 'auto.offset.reset': 'largest' })
-    .then(function(response) {
-      consumerInstance = response[0];
-    })
-    .fail(function(error) {
-      node.error(error);
-    });
-
-    try {
-      this.log("Consumer created...");
-      setInterval(function() {
-        consumerInstance.get(topic)
+    function getTopicAndSend(consumerInstance) {
+      var lastCheck = Date.now();
+      return consumerInstance
+        .get(topic)
         .then(function(data) {
-          for(var i=0; i<data.length; i++) {
+          for (var i=0; i < data.length; i++) {
             node.send({payload: data[i]});
           }
         })
         .fail(function(err) {
           node.error(err);
+        })
+        .then(function() {
+          var deferred = Q.defer();
+          var waitMsec = interval - (Date.now() - lastCheck);
+          setTimeout(function() {
+            deferred.resolve(getTopicAndSend(consumerInstance));
+          }, waitMsec > 0 ? waitMsec : 0)
+          return deferred.promise;
         });
-      }, 2000);
     }
-    catch(e){
-      node.error(e);
-      return;
-    }
+
+    node.log('Creating consumer for topic: \'' + topic + '\'');
+
+    instance
+      .consume('nodered-' + topic + "-" + random(), 'nodered', { 'auto.offset.reset': 'largest' })
+      .then(function(response) {
+        return response[0];
+      })
+      .then(function(consumerInstance) {
+        node.log("Consumer created...");
+        return getTopicAndSend(consumerInstance)
+      })
+      .fail(function(error) {
+        node.error(error);
+      });
   }
 
   RED.nodes.registerType("messagehub in", MessageHubConsumer);
